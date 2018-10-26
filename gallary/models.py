6dc562267a2855e2a25812fb0599a462b8dd68a1
Howick <931063432@qq.com>
2018-10-26 17:27:29 +0800

    创建主页

---
+++ gallary/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Gallary(models.Model):
+    description = models.CharField(default='在这里描述', max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='标题', max_length=50)
+
+    def __str__(self):
+        return self.title
+
