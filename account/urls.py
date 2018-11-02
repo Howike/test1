f9d6ebe174da226b8f8b4aa0f182d23429cd66b1
Howick <931063432@qq.com>
2018-11-02 16:00:11 +0800

    增加登陆与退出功能

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('signup/', views.login, name='登陆页面'),
-    path('loginup/', views.signup, name='注册页面'),
+    path('signup/', views.signup, name='注册页面'),
+    path('login/', views.login, name='登陆页面'),
+    path('logout/', views.logout, name='退出页面')
 ]
